9046fc8bf8faa105eb09856a11721fe6f3be1195
Cesar Augusto <cesinhalius@hotmail.com>
2023-01-19 17:57:32 -0300

    Adicionando função de mudar a página

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode(){
+    const html = document.documentElement
+    html.classList.toggle("light")
+
+    const img = document.querySelector("#profile img");
+
+    if(html.classList.contains("light")){
+        img.setAttribute("src", "./assets/avatar.jpg")
+    } else {
+        // set tiver sem light mode, manter a imagem normal
+        img.setAttribute("src", "./assets/avatar.jpg")
+    }
+}
